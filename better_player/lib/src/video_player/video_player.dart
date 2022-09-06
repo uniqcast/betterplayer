266717da57d9df79d4901bdb217c96e308a06717
Slobodan Klentikov <slobodan.kletnikov@uniqcast.com>
2022-09-06 14:19:42 +0200

    Removed play/pause from seek

--- better_player/lib/src/video_player/video_player.dart
+++ better_player/lib/src/video_player/video_player.dart
@@ -537,13 +537,6 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   /// and silently clamped.
   Future<void> seekTo(Duration? position) async {
     _timer?.cancel();
-    bool isPlaying = value.isPlaying;
-    final int positionInMs = value.position.inMilliseconds;
-    final int durationInMs = value.duration?.inMilliseconds ?? 0;
-
-    if (positionInMs >= durationInMs && position?.inMilliseconds == 0) {
-      isPlaying = true;
-    }
     if (_isDisposed) {
       return;
     }
@@ -551,19 +544,13 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
     Duration? positionToSeek = position;
     if (position! > value.duration!) {
       positionToSeek = value.duration;
-    } else if (position < const Duration()) {
-      positionToSeek = const Duration();
+    } else if (position < Duration.zero) {
+      positionToSeek = Duration.zero;
     }
     _seekPosition = positionToSeek;
 
     await _videoPlayerPlatform.seekTo(_textureId, positionToSeek);
     _updatePosition(position);
-
-    if (isPlaying) {
-      play();
-    } else {
-      pause();
-    }
   }
 
   /// Sets the audio volume of [this].
